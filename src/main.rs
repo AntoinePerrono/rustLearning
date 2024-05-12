bde7d6b816f1d1963b6de92fa20e2f8bd9e2457b
Antoine Perrono <antoine.perrono@cpe.fr>
2024-05-12 02:40:41 +0200

    rust basics first part

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+fn interproduct(a: i32, b: i32, c: i32) -> i32 {
+    return a*b+b*c+c*a;
+}
+
+fn takes_u32(x: u32) {
+    println!("u32: {x}");
+}
+
+fn takes_i8(y: i8) {
+    println!("i8: {y}");
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! it's me, Skafee");
+    let a: i32 = 58;
+    println!("a: {a}");
+    let string: &str = "coucou";
+    println!("{string}");
+    println!("result: {}", interproduct(120, 45, 36));
+    let x = 10;
+    let y = 20;
+
+    takes_u32(x);
+    takes_i8(y);
 }
